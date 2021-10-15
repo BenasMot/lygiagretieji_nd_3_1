c3d15203523bf149c0dc4effaa8ea1a9103c68ed
BenasMot <benasmotiejunas.bm@gmail.com>
2021-10-15 20:55:45 +0300

    idk

--- main.cpp
+++ main.cpp
@@ -1,53 +1,63 @@
-#include <iostream>
-#include <thread>
+#include <algorithm>
 #include <chrono>
+#include <iostream>
 #include <mutex>
+#include <thread>
+#include <vector>
 using namespace std;
 
-void worker(int n);
+void worker(vector<mutex *> mtx, int n);
 void setTimeout(int ms);
+vector<int> randomArray(int size);
 
-mutex my_res;
+int workers = 8;
+int mutex_count = 3;
 
-int main()
-{
-    int workers = 8;
+int main() {
+  vector<mutex *> mutexes(3);
+  for (int i = 0; i < mutex_count; ++i) {
+    mutexes[i] = new mutex();
+  }
 
-    thread tid[workers];
-    for (int i = 0; i < workers; i++)
-    {
-        tid[i] = thread(worker, i);
-    }
+  thread tid[workers];
+  for (int i = 0; i < workers; ++i) {
+    tid[i] = thread(worker, mutexes, i);
+  }
 
-    for (int i = 0; i < workers; ++i)
-    {
-        tid[i].join();
-    }
-    return 0;
+  for (int i = 0; i < workers; ++i) {
+    tid[i].join();
+  }
+
+  for (int i = 0; i < mutex_count; ++i) {
+    delete mutexes[i];
+  }
+
+  return 0;
 }
 
-void worker(int n)
-{
-    // Let them all "work" for a bit
-    setTimeout(1000);
-
-    // If multiple of 4, endless loop
-    bool myBool = true;
-    while(n%4 == 0) {
-        myBool = !myBool;
+void worker(vector<mutex *> mtx, int n) {
+  while (true) {
+    vector<int> random_order = randomArray(mutex_count);
+    for (auto i : random_order) {
+      mtx[i]->lock();
     }
 
-    // If not multiple of 4+1, keep on "working"
-    if(n%4 != 1) {
-        setTimeout(5000);
-    }
+    cout << "Worker " << n << " locked everything" << endl;
+    setTimeout(200);
 
-    my_res.lock();
-    cout << "Thread " << n << " is no longer working " << endl;
-    my_res.unlock();
+    for (auto i : random_order) {
+      mtx[i]->unlock();
+    }
+  }
 }
 
-void setTimeout(int ms)
-{
-    this_thread::sleep_for(chrono::milliseconds(ms));
+void setTimeout(int ms) { this_thread::sleep_for(chrono::milliseconds(ms)); }
+
+vector<int> randomArray(int size) {
+  vector<int> arr(size);
+  for (int i : arr) {
+    arr.push_back(i);
+  }
+  random_shuffle(arr.begin(), arr.end());
+  return arr;
 }
